2995f02a64b3932d6cd82fe91c5f33584df7784f
unknown <arestre@ncsu.edu>
2022-07-04 20:06:43 -0400

    getting signup to work

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
-import { useMutation } from '@apollo/client';
-import { ADD_USER } from '../utils/mutations';
-import Auth from '../utils/auth';
+import React, { useState } from "react";
+import { Form, Button, Alert } from "react-bootstrap";
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
 
 const SignupForm = () => {
   // set initial form state
-  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  const [userFormData, setUserFormData] = useState({
+     username: "",
+     email: "",
+     password: "",
+     });
   // set state for form validation
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
   // mutation for add user
-  const [addUser, { error }] = useMutation(ADD_USER);
+  const [addUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -23,32 +27,16 @@ const SignupForm = () => {
     event.preventDefault();
 
     // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
 
-    try {
-      const response = await addUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('Something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
-    }
-
-    setUserFormData({
-      username: '',
-      email: '',
-      password: '',
+    const mutationData = await addUser({
+      variables: {
+        email: userFormData.email,
+        password: userFormData.password,
+        username: userFormData.username
+      },
     });
+    const token = mutationData.data.addUser.token;
+    Auth.login(token)
   };
 
   return (
